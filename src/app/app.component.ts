848a97904cfac6f7b493631c4957e6b8df51220d
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2023-05-30 22:13:00 -0500

    Main page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ApiTest';
+  items: MenuItem[] = [];
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Characters',
+        routerLink: 'characters'
+      },
+      { label: 'Locations',
+    routerLink: 'locations' },
+      { label: 'Episode',
+    routerLink: 'episodes' }
+
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
+
+//PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { PaginatorModule } from 'primeng/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { LocationsComponent } from './components/locations/locations.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    LocationsComponent,
+    EpisodesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule, ButtonModule, PaginatorModule, BrowserAnimationsModule, MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
