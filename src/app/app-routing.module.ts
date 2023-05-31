55ca8c02f0ecb4ffb467250a4c49cd44748f9db5
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2023-05-30 22:13:11 -0500

    Normal set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './components/characters/characters.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', component: CharactersComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'episodes', component: EpisodesComponent },
+  { path: '**', component: NotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
